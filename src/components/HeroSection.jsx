65335ba6342a918b7e4489824764041deb4a1947
sanyogita <sanyogitavkaps@gmail.com>
2024-08-01 19:42:53 +0530

    changes in herosections

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,12 +4,12 @@ import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 import Link from "next/link";
-import { saveAs } from "file-saver";
+// import { saveAs } from "file-saver";
 
 const HeroSection = () => {
-  const downloadCV = () => {
-    saveAs("/resume/Sanyogita_2023_cv.pdf", "Sanyogita_2023_cv.pdf");
-  };
+  // const downloadCV = () => {
+  //   saveAs("/resume/Sanyogita_2023_cv.pdf", "Sanyogita_2023_cv.pdf");
+  // };
   return (
     <section className="lg:py-16">
       <div className="grid grid-cols-1 sm:grid-cols-12">
@@ -40,8 +40,8 @@ const HeroSection = () => {
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             Experienced in developing interactive web applications using
-            JavaScript, React, Node.js, Nest.js Next.js. Passionate about
-            building user-friendly interfaces and scalable back-end systems.
+            JavaScript, React, Node.js, Nest.js Next.js. Passionate about building
+            user-friendly interfaces and scalable back-end systems.
           </p>
           <div>
             <Link
@@ -50,14 +50,15 @@ const HeroSection = () => {
             >
               Hire Me
             </Link>
-            <button
-              onClick={downloadCV}
+            <a
+              href="/resume/Sanyogita_2023_cv.pdf"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+              download
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
               </span>
-            </button>
+            </a>
           </div>
         </motion.div>
         <motion.div
